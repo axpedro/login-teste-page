bb60721e1dc7eefe8a2ebfaf1cc284191b634c20
axpedro <pedruax@hotmail.com>
2025-05-04 17:29:26 +0200

    Formulario-validators-Service

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { LoginLayoutComponent } from '../../components/login-layout/login-layout.component';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
+import { Router } from '@angular/router';
+import { LoginService } from '../../services/login.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [LoginLayoutComponent, ReactiveFormsModule],
+  imports: [LoginLayoutComponent, ReactiveFormsModule, PrimaryInputComponent],
+  providers: [LoginService],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
@@ -14,14 +19,37 @@ loginForm!: FormGroup;
 
 
 
-  constructor(){
+  constructor(
+private router: Router,
+private loginService: LoginService,
+private toastService: ToastrService
+
+  ){
+
     this.loginForm = new FormGroup({
       email: new FormControl ('', [Validators.required , Validators.email]),
       password: new FormControl ('', [Validators.required, Validators.minLength(6)])
-
-
       
     })
+  }
+
+
+  submit(){
+
+this.loginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
+  next: ()=>this.toastService.success("Login feito com sucesso"),
+
+  error: ()=>this.toastService.error("Erro no Login")
+})
+
+
+
+    console.log(this.loginForm.value);
+
+
+  }
+  navigate(){
+    this.router.navigate(["signup"])
 
 
   }
